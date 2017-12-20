6822889fd1c3501a7afe5fd84d519c84d7bbe383
dawn5527 <790567025@qq.com>
2017-12-20 12:26:48 +0800

    code

--- model.py
+++ model.py
@@ -1,27 +1,52 @@
 import numpy as np
 import torch
-import torch.nn as nn
+import torch.nn as nn 
 import torch.nn.functional as F
 from torch.autograd import Variable
 from torch import optim
 
-
 class LeNet(nn.Module):
     def __init__(self):
-        super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 6, kernel_size=5)#输入和输出通道数分别为1和10
-        self.conv2 = nn.Conv2d(6, 16, kernel_size=5)#输入和输出通道数分别为10和20
-        self.conv2_drop = nn.Dropout2d()#随机选择输入的信道，将其设为0
-        self.fc1 = nn.Linear(16*4*4, 120)#输入的向量大小和输出的大小分别为320和50
+        super(LeNet, self).__init__()
+        #1*28*28->6*24*24
+        self.conv1 = nn.Conv2d(1, 6, kernel_size=5)
+        #6*12*12->16*8*8
+        self.conv2 = nn.Conv2d(6, 16, kernel_size=5)
+        #dropout
+        self.conv2_drop = nn.Dropout2d()
+        #6*4*4->120
+        self.fc1 = nn.Linear(16*4*4, 120)
+        #120->84
         self.fc2 = nn.Linear(120, 84)
+        #84->10
         self.fc3 = nn.Linear(84, 10)
 
     def forward(self, x):
-        x = F.relu(F.max_pool2d(self.conv1(x), 2))#conv->max_pool->relu
-        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))#conv->dropout->max_pool->relu
-        x = x.view(-1, 128)
-        x = F.relu(self.fc1(x))#fc->relu
-        x = F.dropout(x, training=self.training)#dropout
-        x = F.relu(self.fc2(x))  # fc->relu
+        #conv->max_pool->relu
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        #conv->dropout->max_pool->relu
+        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+        x = x.view(-1, 16*4*4)
+        #fc->relu
+        x = F.relu(self.fc1(x))
+        #dropout
+        x = F.dropout(x, training=self.training)
+        # fc->relu
+        x = F.relu(self.fc2(x))  
         x = self.fc3(x)
-        return F.log_softmax(x)
+        return F.log_softmax(x,dim=1)
+    
+    def predict(self, x):
+        #conv->max_pool->relu
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        #conv->dropout->max_pool->relu
+        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+        x = x.view(-1, 16*4*4)
+        #fc->relu
+        x = F.relu(self.fc1(x))
+        #dropout
+        x = F.dropout(x, training=self.training)
+        # fc->relu
+        x = F.relu(self.fc2(x))  
+        x = self.fc3(x)
+        return F.softmax(x,dim=1)
--- train.py
+++ train.py
@@ -1,70 +1,87 @@
+import argparse
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
 from torchvision import datasets, transforms
+
+import torch.optim as optim
 from torch.autograd import Variable
+import torch.nn.functional as F
 
-import model as LeNet
+import model
 
-# 定义超参数
-batch_size = 32
-lr = 1e-3
-momentum = 0.5
-log_interval = 10
-epochs = 20
 
-# # 下载训练集 MNIST 手写数字训练集
-# train_dataset = datasets.MNIST(root='./data', train=True,
-#                                transform=transforms.ToTensor(),
-#                                download=True)
-#
-# test_dataset = datasets.MNIST(root='./data', train=False,
-#                               transform=transforms.ToTensor())
-#
-# train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-# test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+# Training settings
+parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
+parser.add_argument('--batch-size', type=int, default=64, metavar='N',
+                    help='input batch size for training (default: 64)')
+parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
+                    help='input batch size for testing (default: 1000)')
+parser.add_argument('--epochs', type=int, default=10, metavar='N',
+                    help='number of epochs to train (default: 10)')
+parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
+                    help='learning rate (default: 0.01)')
+parser.add_argument('--momentum', type=float, default=0.5, metavar='M',
+                    help='SGD momentum (default: 0.5)')
+parser.add_argument('--no-cuda', action='store_true', default=False,
+                    help='disables CUDA training')
+parser.add_argument('--seed', type=int, default=1, metavar='S',
+                    help='random seed (default: 1)')
+parser.add_argument('--log-interval', type=int, default=10, metavar='N',
+                    help='how many batches to wait before logging training status')
+parser.add_argument('--save-path', type=str, default='./results/lenet.pkl', metavar='P',
+                    help='where to save the trained model')
+args = parser.parse_args()
+args.cuda = not args.no_cuda and torch.cuda.is_available()
 
+torch.manual_seed(args.seed)
+if args.cuda:
+    torch.cuda.manual_seed(args.seed)
+
+
+kwargs = {'num_workers': 2, 'pin_memory': True} if args.cuda else {}
 train_loader = torch.utils.data.DataLoader(
-    minst=datasets.MNIST('../data', train=True, download=True, transform=transforms.Compose([
+    datasets.MNIST('../data', train=True, download=True,
+                   transform=transforms.Compose([
                        transforms.ToTensor(),
-                       transforms.Normalize((0.1307,), (0.3081,)),
-                       transforms.resize(28, 28)
+                       transforms.Normalize((0.1307,), (0.3081,))
                    ])),
-    batch_size=batch_size, shuffle=True)
+    batch_size=args.batch_size, shuffle=True, **kwargs)
 test_loader = torch.utils.data.DataLoader(
     datasets.MNIST('../data', train=False, transform=transforms.Compose([
                        transforms.ToTensor(),
-                       transforms.Normalize((0.1307,), (0.3081,)),
-                       transforms.resize(28, 28)
+                       transforms.Normalize((0.1307,), (0.3081,))
                    ])),
-    batch_size=batch_size, shuffle=False)
+batch_size=args.test_batch_size, shuffle=True, **kwargs)
 
-model = LeNet()
 
-optimizer = optim.SGD(model.parameters(), lr=lr, momentum=momentum)
+model = model.LeNet()
+if args.cuda:
+    model.cuda()
 
+optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
 
 def train(epoch):
     model.train()
     for batch_idx, (data, target) in enumerate(train_loader):
+        if args.cuda:
+            data, target = data.cuda(), target.cuda()
         data, target = Variable(data), Variable(target)
         optimizer.zero_grad()
         output = model(data)
         loss = F.nll_loss(output, target)
         loss.backward()
         optimizer.step()
-        if batch_idx % log_interval == 0:
+        if batch_idx % args.log_interval == 0:
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, batch_idx * len(data), len(train_loader.dataset),
                 100. * batch_idx / len(train_loader), loss.data[0]))
-
 
 def test():
     model.eval()
     test_loss = 0
     correct = 0
     for data, target in test_loader:
+        if args.cuda:
+            data, target = data.cuda(), target.cuda()
         data, target = Variable(data, volatile=True), Variable(target)
         output = model(data)
         test_loss += F.nll_loss(output, target, size_average=False).data[0] # sum up batch loss
@@ -77,6 +94,10 @@ def test():
         100. * correct / len(test_loader.dataset)))
 
 
-for epochi in range(1, epochs + 1):
-    train(epochi)
+for epoch in range(1, args.epochs + 1):
+    train(epoch)
     test()
+
+# save results
+torch.save(model.state_dict(), args.save_path)
+print("The model has been saved in [{}]".format(args.save_path))
